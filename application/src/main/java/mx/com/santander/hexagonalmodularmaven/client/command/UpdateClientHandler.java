f57924d6ff6ee68464149c5fcd8e0b4c8c4c7ba7
IsaacGM00 <igmendez@minsait.com>
2025-07-10 15:16:45 -0600

    Se agregó la actualización del producrhandler

--- application/src/main/java/mx/com/santander/hexagonalmodularmaven/client/command/UpdateClientHandler.java
+++ application/src/main/java/mx/com/santander/hexagonalmodularmaven/client/command/UpdateClientHandler.java
@@ -4,10 +4,18 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 import mx.com.santander.hexagonalmodularmaven.client.mapper.ClientMapperDto;
+import mx.com.santander.hexagonalmodularmaven.client.model.dto.DtoClient;
+import mx.com.santander.hexagonalmodularmaven.client.model.entity.Client;
+import mx.com.santander.hexagonalmodularmaven.client.service.UpdateClientService;
 
 @Component
 @RequiredArgsConstructor
 public class UpdateClientHandler {
+
     private final ClientMapperDto clientMapperDto;
-    
+    private final UpdateClientService updateClientService;
+
+    public DtoClient execute(Long id, Client updaClient){
+        return clientMapperDto.toDto(updateClientService.execute(id, updaClient));
+    }
 }
